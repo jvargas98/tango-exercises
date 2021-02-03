13122f710843a033b88bde7b1430539de7186457
Jorge <jvargas18@ucol.mx>
2021-02-03 11:40:46 -0600

    Hashes

---
+++ ruby/pickaxe/hash.rb
@@ -0,0 +1,11 @@
+h = { 'dog' => 'canine', 'cat' => 'feline', 'donkey' => 'asinine' }
+
+p h.length # => 3
+p h['dog'] # => "canine"
+h['cow'] = 'bovine'
+h[12] = 'dodecine'
+h['cat'] = 99
+p h
+
+j = { dog: 'canine', cat: 'feline', donkey: 'asinine' }
+p j
