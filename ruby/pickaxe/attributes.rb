980356e1023ca51f2f9eee2f002b1307ae557eb1
Jorge <jvargas18@ucol.mx>
2021-02-02 23:35:33 -0600

    Writable attributes

--- ruby/pickaxe/attributes.rb
+++ ruby/pickaxe/attributes.rb
@@ -14,3 +14,17 @@ end
 book = BookInStock.new("isbn1", 12.34)
 puts "ISBN = #{book.isbn}"
 puts "Price = #{book.price}"
+
+# With accessor methods
+
+class BookInStock
+  attr_reader :isbn, :price
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = Float(price)
+  end
+end
+
+book = BookInStock.new("isbn1", 12.34)
+puts "ISBN = #{book.isbn}"
+puts "Price = #{book.price}"
